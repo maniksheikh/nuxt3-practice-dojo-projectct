854838d1531dedbe545eb73f2a2267defb1520cb
maniksheikh <mdmanik081498@gmail.com>
2023-09-11 13:14:47 +0600

    added the error page

---
+++ error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mt-7 max-w-sm mx-auto text-center card border-2 bg-white pb-5">
+    <p class="mt-7 text-8xl font bold">{{ error.statusCode }}</p>
+    <p class="mt-7 text-6xl">Ooops.</p>
+    <p class="mt-7 text-xl pb-3">{{ error.message }}</p>
+    <button class="p-1 bg-green-600 text-white rounded-md" @click="handleClearError">Go Home....</button>
+  </div>
+</template>
+
+<script setup>
+defineProps(["error"]);
+
+const handleClearError = () => clearError({ redirect: '/'})
+
+</script>
+
+<style scoped>
+</style>
